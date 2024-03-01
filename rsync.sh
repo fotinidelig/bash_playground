02a065e4d096e28ba4515806e9e35904af5b79d3
Fotini Deligiannaki <44481663+fotinidelig@users.noreply.github.com>
2024-03-01 16:22:56 +0100

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -5,4 +5,4 @@ DEST_USER="remote_user"
 DEST_HOST="remote_hostname"
 DEST_DIR="remote_dir"
 
-rsync -avz --no-p -e ssh "$SOURCE_DIR" "$DEST_USER@$DEST_HOST:$DEST_DIR"
+rsync -avz --ignore-errors --ignore-existing --no-p -e ssh "$SOURCE_DIR" "$DEST_USER@$DEST_HOST:$DEST_DIR"
