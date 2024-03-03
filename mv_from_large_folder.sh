7351d9092a83dcad8641c9ea6da923253ca21cd2
Fotini Deligiannaki <44481663+fotinidelig@users.noreply.github.com>
2024-03-03 10:51:53 +0100

    Update mv_from_large_folder.sh

--- mv_from_large_folder.sh
+++ mv_from_large_folder.sh
@@ -1,8 +1,11 @@
 #!/bin/bash/
 
-source_dir="dir"
-destination_dir="ddir"
-n_samples=10000
+# move first n_samples files from source to dir
+# works with arbitrary high number of files in dir
+
+source_dir=$1
+destination_dir=$2
+n_samples=$3
 
 cd $source_did
 ls | head -n $n_samples | xargs -I{} mv {} $destination_dir
